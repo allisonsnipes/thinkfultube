9063f2741f2a8260ff92c4a14226cb1d35300057
allisonsnipes <ajsnipes@usfca.edu>
2018-11-25 11:40:42 -0800

    working on the DOM

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict" //always put use stript
+
+
+$(document).ready(function(){ //always use document ready function to start
+
+});
+
+$(".submitButton").on("click", event => { //listner for user clicking submit 
+    event.preventDefault();
+}) 
