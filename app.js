66740c3a59901f199628ec712898f0dd0eca7bbf
allisonsnipes <ajsnipes@usfca.edu>
2018-11-28 02:07:40 -0800

    updating dom

--- app.js
+++ app.js
@@ -2,9 +2,23 @@
 
 
 $(document).ready(function(){ //always use document ready function to start
+    generateSearch();
+    $(".populatedSearch").hide();
+    const key = 'AIzaSyBfo5FDsAC78Rrqd5cjrGu4L4UUBUqZp44';
+    const URL = '';
 
 });
 
+
 $(".submitButton").on("click", event => { //listner for user clicking submit 
     event.preventDefault();
-}) 
+
+});
+
+function generateSearch() { //generates the YouTube search from DOM manipulation
+    $(".populatedSearch").html(`
+        <a href="#" class="previousResults round"> &#8249; </a>
+        <a href="#" class="nextResults round"> &#8250; </a>
+    
+    `);
+}
